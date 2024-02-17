b394511eabc098ce2a88d2d44ea8cae3a602316d
guowei1berry <guowei1berry@gmail.com>
2024-02-18 00:29:03 +0800

    Success to dynamically identify header and skip rows and concatenate

--- concat2_withsearch/smupandas.py
+++ concat2_withsearch/smupandas.py
@@ -1,97 +1,42 @@
 import pandas as pd
 from dateutil.parser import parse
 
-# Read the CSV file
-# data1 = pd.read_csv("table1.csv",skiprows=4)
-# data2 = pd.read_csv("table2.csv",header=4) #skiprows=4,index_col=None)#,skiprows=5)
-# cols = ['TIMESTAMP','RECORD','BattV','PTemp_C','T108_C']
-# cols = [0,2,3,4]
-
-
-
 data1 = pd.read_csv("table1.csv",nrows=10) #header index is start from 0, so this means 2nd row
 data2 = pd.read_csv("table2.csv",nrows=10)#usecols = cols) #skiprows=4,index_col=None)#,skiprows=5)
-
-
-# print (data1.iat[3,0])
-theobject =data1.iat[2,0]
-# print(parse(theobject))
-try: 
-    if parse(theobject):
-        print(parse(theobject))
-        print('is dateobject')
-    # else: pass
-except:
-    pass
 
 ##identifying HEADER row
 wordsearch = 'TIMESTAMP'
 
 def findHeaderrow(dataframe):
-
     i = 0
     while i < 5:
         if (dataframe.iat[i,0]) == str(wordsearch):
-            # print (dataframe.iat[i,0])
-            header_row = i + 1 
-            # print ("header_row",headeconcat2_withsearchr_row)
+            header_row = [i + 1] 
+
             return header_row
 
         else: i +=1
 
 def findSkiprows_2ndfile(dataframe):
-    header_index = findHeaderrow(dataframe)
-    print(header_index)
-    x=header_index +1
-    print (x)
+    header_index = findHeaderrow(dataframe)[0] #header row considered index 1
+    x=header_index # Skiprow counting starts from index0 / header row is indexed 1 => add 1 to account for index 0
     array =[]
-    print(array)
-    # print(parse(theobject))
+
     while x<10:
         theobject =data1.iat[x,0]
-        print(theobject)
         try: 
             if parse(theobject):
-                print(parse(theobject))
-                print('is dateobject')
-                skiparray = header_index - x #[index to skip]
-                
-                array = array + skiparray
-                print(array)
                 return array
             # else: pass
         except:
+            array.append(x +1)     #error parsing, appends index to array
             pass
         x += 1
-    return
+    return array
 
-findSkiprows_2ndfile(data2)
-
-data1_header = pd.read_csv("table1.csv",header=[findHeaderrow(data1)]) #header index is start from 0, so this means 2nd row
-data2 = pd.read_csv("table2.csv",header=[findHeaderrow(data2)],skiprows=[2,3])#usecols = cols) #skiprows=4,index_col=None)#,skiprows=5)
+data1_header = pd.read_csv("table1.csv",header=findHeaderrow(data1)) #header index is start from 0, so this means 2nd row
+data2_skiprows = pd.read_csv("table2.csv",header=findHeaderrow(data2),skiprows=findSkiprows_2ndfile(data2))#usecols = cols) #skiprows=4,index_col=None)#,skiprows=5)
 
 
-# View the first 5 rows
-# print ("table1")
-# first_row = data1.loc[data1.index[0]]
-# print(data1.iloc[:0])
-# print (first_row)
-# print (data1.head())
-# print ("table2")
-# print (data2.head())
-
-# data1.columns=data2.columns
-
-# data3 = pd.concat([data1,data2],ignore_index=True, axis=0)
-# data4 = data1.iloc[:0]
-
-# print(data1.iat[0,0])#timestamp
-# print(data1.iat[1,0])#TS
-
-# print('results')
-# print (data3)
-# data3 = pd.concat([data1,data2],ignore_index=True)#,join='inner' ,ignore_index=True)
-
-# data3.to_csv("concat_normal_columns.csv")
-# data4.to_csv("iloc.csv")
-# data2.head()
+data3 = pd.concat([data1_header,data2_skiprows],ignore_index=True, axis=0)
+data3.to_csv("concat.csv")
