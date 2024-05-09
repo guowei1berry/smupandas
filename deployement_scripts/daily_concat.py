70bedaf5036d53d34d4f93edaaa233720e3dcab5
guowei1berry <guowei1berry@gmail.com>
2024-05-09 14:06:01 +0800

    concat horizontal still some errors : raise ValueError(f"cannot insert {column}, already exists")

--- deployement_scripts/daily_concat.py
+++ deployement_scripts/daily_concat.py
@@ -12,8 +12,14 @@ dailysetfolder_path = '/home/ubuntu2004/CODE/smu_data/daily_compiled/dailyset/20
 dailysetfolder = os.listdir(dailysetfolder_path)
 sorted_dailyset = sorted(dailysetfolder)
 
+#eliminate files with .~lock prefix
+for eachFile in sorted_dailyset:
+    print('eachFile[0:6]',eachFile[0:6])
+    if eachFile[0:6]=='.~lock':
+        print('locking',eachFile,f'{dailysetfolder_path}/{eachFile}')
+        os.remove(f'{dailysetfolder_path}/{eachFile}')
 
-##output Excel with multiple sheets and concatenated data
+#output Excel with multiple sheets and concatenated data
 with pd.ExcelWriter('output.xlsx', engine='openpyxl') as writer: #engine='xlsxwriter') as writer: 
     # Write each DataFrame to a separate worksheet #sheetname max 31 char
     for eachFile in sorted_dailyset:
@@ -21,3 +27,29 @@ with pd.ExcelWriter('output.xlsx', engine='openpyxl') as writer: #engine='xlsxwr
         print('sliced',sliced)
         df = pd.read_csv(f'{dailysetfolder_path}/{eachFile}')
         df.to_excel(writer, sheet_name=f"{sliced}", index=True)
+
+#Concat horizontally
+print('sorted_dailyset',sorted_dailyset)
+
+for eachFile in sorted_dailyset:
+    
+
+    print('eachfile', eachFile)
+    readCurrentFile = pd.read_csv(f'{dailysetfolder_path}/{eachFile}',header=[1],index_col=0)#,header=[1],skiprows=[2,3])#findHeaderrow(data1)) #header index is start from 0, so this means 2nd row
+    print('currenthead', readCurrentFile.head)
+    slicedCurrent = eachFile[0:9]
+    readCurrentFile.insert(0, f'{slicedCurrent}=>', f'{slicedCurrent}=>')
+    
+    
+    if os.path.isfile('/home/ubuntu2004/CODE/smu_env/deployement_scripts/concatedHoriz.csv'):
+        readPrevFile = pd.read_csv('concatedHoriz.csv',header=[1],index_col=0)#usecols = cols) #skiprows=4,index_col=None)#,skiprows=5)
+        slicedPrev = prevFile[0:9]
+        readPrevFile.insert(0, f'{slicedPrev}=>', f'{slicedPrev}=>')
+        print("readPrevFile",readPrevFile.head)
+        data3 = pd.concat([readPrevFile,readCurrentFile],ignore_index=False, axis=1,join='outer') # needs right header for concat to work
+        data3.to_csv("concatedHoriz.csv")
+    else:
+        readCurrentFile = pd.read_csv(f'{dailysetfolder_path}/{eachFile}',header=[1],index_col=0)#,header=[1],skiprows=[2,3])#findHeaderrow(data1)) #header index is start from 0, so this means 2nd row
+        readCurrentFile.to_csv("concatedHoriz.csv")
+        prevFile = eachFile
+        print('prevFile',prevFile)
--- deployement_scripts/smupandas_concat_clean_horiz.py
+++ deployement_scripts/smupandas_concat_clean_horiz.py
@@ -3,15 +3,20 @@
 import pandas as pd
 from dateutil.parser import parse
 
+eachFile = "SchoolA_1_Roof_Daily_2024-04-19_00-01-00.277.csv"
 # data1 = pd.read_csv("3.csv",nrows=10) #header index is start from 0, so this means 2nd row
-data1 = pd.read_csv("SchoolA_1_Roof_Daily_2024-04-19_00-01-00.277.csv",header=[1],index_col=0)#,header=[1],skiprows=[2,3])#findHeaderrow(data1)) #header index is start from 0, so this means 2nd row
+data1 = pd.read_csv(eachFile,header=[1],index_col=0)#,header=[1],skiprows=[2,3])#findHeaderrow(data1)) #header index is start from 0, so this means 2nd row
 # data1['FileName'] = 'data1'
-data1.insert(0, 'Data1', 'data1')
+sliced = eachFile[0:9]
+data1.insert(0, f'{sliced}=>', f'{sliced}=>')
 # data1.at[2, 'C'] = 'data1'
 print("data1",data1.head)
-data2 = pd.read_csv("SchoolA_2_Daily_2024-04-19_00-01-00.906.csv",header=[1],index_col=0)#usecols = cols) #skiprows=4,index_col=None)#,skiprows=5)
+
+eachFile2= "SchoolA_2_Daily_2024-04-19_00-01-00.906.csv"
+data2 = pd.read_csv(eachFile2,header=[1],index_col=0)#usecols = cols) #skiprows=4,index_col=None)#,skiprows=5)
 # data2['FileName'] = 'data2'
-data2.insert(0, 'Data2', 'data2')
+sliced2 = eachFile2[0:9]
+data2.insert(0, f'{sliced2}=>', f'{sliced2}=>')
 # data2.at[2, 'C'] = 'data2'
 print("data2",data2.head)
 data3 = pd.concat([data1,data2],ignore_index=False, axis=1,join='outer') # needs right header for concat to work
