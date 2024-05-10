70a1dd5a16a7ab6b8d9fb54c047fd61722fe811b
guowei1berry <guowei1berry@gmail.com>
2024-05-10 12:35:03 +0800

    all stations in output.xlsx

--- deployement_scripts/daily_concat.py
+++ deployement_scripts/daily_concat.py
@@ -5,8 +5,9 @@ import shutil
 import pandas as pd
 import xlsxwriter
 
+dateSet = '2024-04-17'
 # Specify the path to the folder
-dailysetfolder_path = '/home/ubuntu2004/CODE/smu_data/daily_compiled/dailyset/2024-04-17'
+dailysetfolder_path = f'/home/ubuntu2004/CODE/smu_data/daily_compiled/dailyset/{dateSet}'
 
 # Get a list of all files and directories in the folder
 dailysetfolder = os.listdir(dailysetfolder_path)
@@ -19,27 +20,10 @@ for eachFile in sorted_dailyset:
         print('locking',eachFile,f'{dailysetfolder_path}/{eachFile}')
         os.remove(f'{dailysetfolder_path}/{eachFile}')
 
-#output Excel with multiple sheets and concatenated data
-with pd.ExcelWriter('output.xlsx', engine='openpyxl') as writer: #engine='xlsxwriter') as writer: 
-    # Write each DataFrame to a separate worksheet #sheetname max 31 char
-    for eachFile in sorted_dailyset:
-        sliced = eachFile[6:31]
-        print('sliced',sliced)
-        df = pd.read_csv(f'{dailysetfolder_path}/{eachFile}')
-        df.to_excel(writer, sheet_name=f"{sliced}", index=True)
-
 #Concat horizontally
 print('sorted_dailyset',sorted_dailyset)
 
 for eachFile in sorted_dailyset:
-    
-
-    # print('eachfile', eachFile)
-    # readFirstFile = pd.read_csv(f'{dailysetfolder_path}/{eachFile}',header=[1],index_col=0)#,header=[1],skiprows=[2,3])#findHeaderrow(data1)) #header index is start from 0, so this means 2nd row
-    # print('currenthead', readFirstFile.head)
-    
-    # readFirstFile.insert(0, f'{slicedCurrent}=>', f'{slicedCurrent}=>')
-    # print('readFirstFile', readFirstFile.head)
 
     if os.path.isfile('/home/ubuntu2004/CODE/smu_env/deployement_scripts/concatedHoriz.csv') ==False:
         readFirstFile = pd.read_csv(f'{dailysetfolder_path}/{eachFile}',header=[1],index_col=0)#,header=[1],skiprows=[2,3])#findHeaderrow(data1)) #header index is start from 0, so this means 2nd row
@@ -60,18 +44,15 @@ for eachFile in sorted_dailyset:
         data3 = pd.concat([readPrevFile,readCurrentFile],ignore_index=False, axis=1,join='outer') # needs right header for concat to work
         data3.to_csv("concatedHoriz.csv")
 
-    # if os.path.isfile('/home/ubuntu2004/CODE/smu_env/deployement_scripts/concatedHoriz.csv'):
-    #     print('prevFile>>>',prevFile)
-    #     readPrevFile = pd.read_csv('concatedHoriz.csv',header=[1],index_col=0)#usecols = cols) #skiprows=4,index_col=None)#,skiprows=5)
-    #     slicedPrev = prevFile[0:9]
-    #     readPrevFile.insert(0, f'{slicedPrev}=>', f'{slicedPrev}=>')
-    #     print("readPrevFile",readPrevFile.head)
-    #     data3 = pd.concat([readPrevFile,readFirstFile],ignore_index=False, axis=1,join='outer') # needs right header for concat to work
-    #     data3.to_csv("concatedHoriz.csv")
-    # else:
-    #     readFirstFile = pd.read_csv(f'{dailysetfolder_path}/{eachFile}',header=[1],index_col=0)#,header=[1],skiprows=[2,3])#findHeaderrow(data1)) #header index is start from 0, so this means 2nd row
-    #     slicedCurrent = eachFile[0:9]
-    #     readFirstFile.insert(0, f'{slicedCurrent}=>', f'{slicedCurrent}=>')
-    #     readFirstFile.to_csv("concatedHoriz.csv")
-    #     prevFile = eachFile
-    #     print('prevFile',prevFile)
+#output Excel with multiple sheets and concatenated data
+with pd.ExcelWriter('output.xlsx', engine='openpyxl') as writer: #engine='xlsxwriter') as writer: 
+    
+    concatdf = pd.read_csv("concatedHoriz.csv") 
+    concatdf.to_excel(writer, sheet_name=f"ALL-STATIONS-{dateSet}", index=True)
+    # Write each DataFrame to a separate worksheet #sheetname max 31 char
+    for eachFile in sorted_dailyset:
+        sliced = eachFile[6:31]
+        print('sliced',sliced)
+        df = pd.read_csv(f'{dailysetfolder_path}/{eachFile}')
+        df.to_excel(writer, sheet_name=f"{sliced}", index=True)
+
